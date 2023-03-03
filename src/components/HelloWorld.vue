f2cd12672cd839b847fb6cea81a177f120e90d84
romulospl <romulopereiralopes@gmail.com>
2023-03-03 12:34:07 -0300

    adicionado pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,27 @@
 <script setup>
+import { mapActions, storeToRefs } from 'pinia';
 import { ref } from 'vue'
+import { useMainStore } from '../store/main';
 
 defineProps({
   msg: String,
 })
 
+const mainStore = useMainStore()
+
 const count = ref(0)
+
+const { counter } = storeToRefs(mainStore)
+const { increment } = mapActions(useMainStore, ["increment"])
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+
+    <button type="button" @click="increment">count is {{ counter }}</button>
+
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
+import { createPinia } from 'pinia'
 
-
+// const pinia = createPinia()
 
 createApp(App)
-.use(router)
-.mount('#app')
+    .use(router)
+    .use(createPinia())
+    .mount('#app')
